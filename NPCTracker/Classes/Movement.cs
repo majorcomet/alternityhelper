32531ad6a09b1e0aed4a3f6545a46295a533276e
majorcomet <majorcomet@gmail.com>
2012-12-22 06:03:20 -0500

    Added 'movement' class and 'Go.png' image

---
+++ NPCTracker/Classes/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alternity {
+  public class Movement {
+    public int Sprint { get; set; }
+    public int Run { get; set; }
+    public int Walk { get; set; }
+    public int EasySwim { get; set; }
+    public int Swim { get; set; }
+    public int Glide { get; set; }
+    public int Fly { get; set; }
+  }
+}
