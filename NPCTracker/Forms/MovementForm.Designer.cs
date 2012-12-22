67796b65e507a2d0a7fba746c773643cc204c90a
majorcomet <majorcomet@gmail.com>
2012-12-22 07:46:32 -0500

    Changed Movement Form to fixed size

--- NPCTracker/Forms/MovementForm.Designer.cs
+++ NPCTracker/Forms/MovementForm.Designer.cs
@@ -248,6 +248,7 @@ namespace Alternity.Forms {
       this.Controls.Add(this.label2);
       this.Controls.Add(this.SprintBox);
       this.Controls.Add(this.label1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.KeyPreview = true;
       this.MaximizeBox = false;
