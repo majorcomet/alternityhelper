6e8e67dc5a3f1be8afbab8e749b266f0f72b11fe
majorcomet <majorcomet@gmail.com>
2012-12-22 06:11:39 -0500

    Added Movement Form

---
+++ NPCTracker/Forms/MovementForm.Designer.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Alternity RPG NPC Tracker/Helper
+ * By Andrew Barber.
+ *
+ * Licensed: CC BY-NC 3.0
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ *
+ * More info at the Github repo:  https://github.com/majorcomet/alternityhelper/wiki
+ */
+namespace Alternity.Forms {
+  partial class MovementForm {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MovementForm));
+      this.SuspendLayout();
+      // 
+      // MovementForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(284, 262);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "MovementForm";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+      this.Text = "Movement";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
---
+++ NPCTracker/Forms/MovementForm.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Alternity RPG NPC Tracker/Helper
+ * By Andrew Barber.
+ *
+ * Licensed: CC BY-NC 3.0
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ *
+ * More info at the Github repo:  https://github.com/majorcomet/alternityhelper/wiki
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Alternity.Forms {
+  public partial class MovementForm : Form {
+    public MovementForm() {
+      InitializeComponent();
+    }
+  }
+}
