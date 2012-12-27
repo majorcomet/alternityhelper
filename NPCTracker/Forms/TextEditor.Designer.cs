d0c7476af0c280e46fdac827975a81637f976899
majorcomet <majorcomet@gmail.com>
2012-12-27 09:41:01 -0500

    Added

---
+++ NPCTracker/Forms/TextEditor.Designer.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * Alternity RPG NPC Tracker/Helper
+ * By Andrew Barber.
+ *
+ * Licensed: CC BY-NC 3.0
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ *
+ * More info at the Github repo:  https://github.com/majorcomet/alternityhelper/wiki
+ */
+namespace Alternity.Forms {
+  partial class TextEditor {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextEditor));
+      this.EditorBox = new System.Windows.Forms.TextBox();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.useDefaultToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+      this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.closeDontSaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // EditorBox
+      // 
+      this.EditorBox.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.EditorBox.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.EditorBox.Location = new System.Drawing.Point(0, 24);
+      this.EditorBox.MaxLength = 2000000;
+      this.EditorBox.Multiline = true;
+      this.EditorBox.Name = "EditorBox";
+      this.EditorBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.EditorBox.Size = new System.Drawing.Size(808, 389);
+      this.EditorBox.TabIndex = 0;
+      this.EditorBox.TextChanged += new System.EventHandler(this.EditorBox_TextChanged);
+      this.EditorBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.EditorBox_KeyDown);
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(808, 24);
+      this.menuStrip1.TabIndex = 1;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.useDefaultToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.saveToolStripMenuItem,
+            this.closeDontSaveToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+      this.fileToolStripMenuItem.Text = "&File";
+      // 
+      // useDefaultToolStripMenuItem
+      // 
+      this.useDefaultToolStripMenuItem.Name = "useDefaultToolStripMenuItem";
+      this.useDefaultToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.useDefaultToolStripMenuItem.Text = "&Use Default...";
+      this.useDefaultToolStripMenuItem.Click += new System.EventHandler(this.useDefaultToolStripMenuItem_Click);
+      // 
+      // toolStripMenuItem2
+      // 
+      this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+      this.toolStripMenuItem2.Size = new System.Drawing.Size(149, 6);
+      // 
+      // saveToolStripMenuItem
+      // 
+      this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+      this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.saveToolStripMenuItem.Text = "&Save";
+      this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+      // 
+      // closeDontSaveToolStripMenuItem
+      // 
+      this.closeDontSaveToolStripMenuItem.Name = "closeDontSaveToolStripMenuItem";
+      this.closeDontSaveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.closeDontSaveToolStripMenuItem.Text = "&Close";
+      this.closeDontSaveToolStripMenuItem.Click += new System.EventHandler(this.closeDontSaveToolStripMenuItem_Click);
+      // 
+      // TextEditor
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(808, 413);
+      this.Controls.Add(this.EditorBox);
+      this.Controls.Add(this.menuStrip1);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MainMenuStrip = this.menuStrip1;
+      this.MinimizeBox = false;
+      this.MinimumSize = new System.Drawing.Size(220, 250);
+      this.Name = "TextEditor";
+      this.Text = "Editor";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox EditorBox;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem useDefaultToolStripMenuItem;
+    private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+    private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem closeDontSaveToolStripMenuItem;
+  }
+}
---
+++ NPCTracker/Forms/TextEditor.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Alternity RPG NPC Tracker/Helper
+ * By Andrew Barber.
+ *
+ * Licensed: CC BY-NC 3.0
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ *
+ * More info at the Github repo:  https://github.com/majorcomet/alternityhelper/wiki
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Alternity.Forms {
+  public partial class TextEditor : Form {
+    private string DefaultFilePath = "";
+    private bool Dirty = false;
+    private string FilePath = "";
+    public TextEditor() {
+      InitializeComponent();
+      this.FormClosing += TextEditor_FormClosing;
+    }
+
+    public TextEditor(string title, string filePath, string defaultFilePath)
+      : this() {
+      this.Text = title;
+      LoadFile(filePath);
+      FilePath = filePath;
+      DefaultFilePath = defaultFilePath;
+    }
+
+    private void EditorBox_TextChanged(object sender, EventArgs e) {
+      Dirty = true;
+    }
+
+    private void LoadFile(string filePath) {
+      try {
+        EditorBox.Text = File.ReadAllText(filePath);
+      } catch (Exception ex) {
+        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+      Dirty = false;
+    }
+
+    private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
+      File.WriteAllText(FilePath, EditorBox.Text);
+      Dirty = false;
+    }
+
+    void TextEditor_FormClosing(object sender, FormClosingEventArgs e) {
+      if (Dirty) {
+        var res = MessageBox.Show("Save data file?", "Closing", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+        if (res == System.Windows.Forms.DialogResult.Yes) {
+          saveToolStripMenuItem_Click(saveToolStripMenuItem, null);
+          this.DialogResult = System.Windows.Forms.DialogResult.OK;
+        } else if (res == System.Windows.Forms.DialogResult.Cancel) {
+          e.Cancel = true;
+        } else if (res == System.Windows.Forms.DialogResult.No) {
+          this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+      } else {
+        this.DialogResult = System.Windows.Forms.DialogResult.OK;
+      }
+    }
+    private void useDefaultToolStripMenuItem_Click(object sender, EventArgs e) {
+      if (MessageBox.Show("This will reload the default values.", "Confirm", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+        == System.Windows.Forms.DialogResult.OK) {
+        LoadFile(DefaultFilePath);
+        Dirty = true;
+      }
+    }
+
+    private void closeDontSaveToolStripMenuItem_Click(object sender, EventArgs e) {
+      this.Close();
+    }
+
+    private void EditorBox_KeyDown(object sender, KeyEventArgs e) {
+      if (e.KeyCode == Keys.A && e.Modifiers == Keys.Control) {
+        EditorBox.SelectAll();
+        e.Handled = true;
+      }
+    }
+  }
+}
